48653dc7b60b21d9ebc8c82a0bf9735114b3135d
Prometheus <105338051+AlRzRz@users.noreply.github.com>
2025-09-15 03:46:59 +0400

    randomWalkDownWallSt start

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+
+
+def nameDoubled(name):
+    return len(name) * 2
+
 
 
 def main():
     titanic = pd.read_csv('./titanic.csv', index_col=0)
-    print(titanic.loc[[12, 23]])
 
 
+    titanic['name length'] = titanic['Name'].apply(nameDoubled)
+    print(titanic)
+
+
+    # for label, row in titanic.iterrows():
+    #     print(f'Label: {label}')
+    #     print(row)
 
 
 
---
+++ randomwalks.py
@@ -0,0 +1,8 @@
+
+
+# TODO Implementation of a random walk func to simulate a trader's pot size based on his win-rate (hacker statistics basically)
+
+
+
+def randomWalkDownWallSt(winRate, simulations):
+    pass
